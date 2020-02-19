aeced51c9930f92834f8dccbeb40abe28d45acf5
dhifaagh <mac@mac.local>
2020-02-19 21:55:34 +0700

    ulululu

--- src/Redux/Actions/categories.js
+++ src/Redux/Actions/categories.js
@@ -4,7 +4,7 @@ const URL_STRING = "3.85.4.188:3333";
 export const categories = () => {
     return {
         type: "GET_CATEGORIES",
-        payload: axios.get(`http://$${URL_STRING}/api/categories`, {
+        payload: axios.get(`http://${URL_STRING}/api/categories`, {
             headers: {
                 Authorization: localStorage.getItem("keyToken")
             }
