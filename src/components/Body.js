56b468fa47964194caa4404beb2e213e367ac57b
dhifaagh <mac@mac.local>
2020-02-20 01:41:27 +0700

    coba

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Redirect } from 'react-router-dom'
 // import "../assets/css/bootstrap.min.css";
 // import "../assets/css/fontawesome.min.css";
 import "../assets/style/body.css";
