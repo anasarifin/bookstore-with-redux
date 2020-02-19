ddb16a2839052f304f280ed523e4286db3a7d0a5
dhifaagh <mac@mac.local>
2020-02-20 02:27:43 +0700

    after reset

--- src/components/Navbars.js
+++ src/components/Navbars.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../assets/style/navbar.css";
+import { Link } from 'react-router-dom';
 
 class Navbars extends Component {
     constructor(props) {
